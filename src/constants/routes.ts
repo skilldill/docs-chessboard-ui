ec5a5cafe7dfa3ef8be89a06ce03d5056fa98c15
Alexander <skilldill@yandex.ru>
2024-06-27 22:13:13 +0300

    update docs

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,16 @@
 export const MENU_ROUTES = [
-    { text: "Get started", link: "/docs/getStarted" },
+    { text: "Getting started", header: true },
+    { text: 'Introduction', link: '/docs/introduction' },
+    { text: "Quick start", link: "/docs/quickStart" },
+
+    { text: "Properties", header: true },
+    { text: "FEN", link: "/docs/fen" },
+    { text: "reversed (optional)", link: "/docs/reversed" },
+    { text: "onChange", link: "/docs/onChange" },
+    { text: "onEndGame", link: "/docs/onEndGame" },
+    { text: "change (optional)", link: "/docs/change" },
+    { text: "config (optional)", link: "/docs/config" },
+
     { text: "Configurations", header: true },
     { text: "Reversing board", link: "/docs/reversingBoard" },
     { text: "Custom FEN", link: "/docs/customFen" },
