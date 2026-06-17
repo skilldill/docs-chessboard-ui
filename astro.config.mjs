cf98181e53b6fc7b494d7276b7a77f5ee0a8a501
Alexander <skilldill@yandex.ru>
2026-06-17 17:12:56 +0300

    updates for docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,15 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://react-chessboard-ui.dev',
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      defaultColor: false,
+    },
+  },
   integrations: [react(), mdx(), sitemap()],
   devToolbar: {
     enabled: false
